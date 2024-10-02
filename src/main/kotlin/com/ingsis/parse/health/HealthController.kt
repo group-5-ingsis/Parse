0a0ef499aeed901a3ef775b7f65913374f570698
husseymarcos <mhussey@mail.austral.edu.ar>
2024-10-02 18:40:38 -0300

    feat: Health controller + test

---
+++ src/main/kotlin/com/ingsis/parse/health/HealthController.kt
@@ -0,0 +1,17 @@
+package com.ingsis.parse.health
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+
+  @GetMapping
+  fun checkHealth(): ResponseEntity<String> {
+    return ResponseEntity("Service is running", HttpStatus.OK)
+  }
+}
