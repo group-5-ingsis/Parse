5bd3605d79ba20534dbfbc8a12a4106496cc876a
husseymarcos <mhussey@mail.austral.edu.ar>
2024-10-02 18:51:07 -0300

    fix: health test

--- src/test/kotlin/com/ingsis/parse/health/HealthControllerTest.kt
+++ src/test/kotlin/com/ingsis/parse/health/HealthControllerTest.kt
@@ -2,20 +2,21 @@ package com.ingsis.parse.health
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import kotlin.test.Test
 
 @WebMvcTest(HealthController::class)
-class HealthControllerTest(@Autowired val mockMvc: MockMvc) {
+class HealthControllerTest {
+
+  @Autowired
+  lateinit var mockMvc: MockMvc
 
   @Test
   fun `checkHealth should return OK status with a message`() {
     mockMvc.get("/health")
       .andExpect {
         status { isOk() }
-        content { contentType(MediaType.APPLICATION_JSON) }
         content { string("Service is running") }
       }
   }
