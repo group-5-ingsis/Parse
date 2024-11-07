708f8e7f1b16bb54052735d48a24dea42e02eb03
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-07 14:00:05 -0300

    fix local build

--- src/main/kotlin/com/ingsis/parse/Parse.kt
+++ src/main/kotlin/com/ingsis/parse/Parse.kt
@@ -11,11 +11,17 @@ open class Parse
 fun main(args: Array<String>) {
   val dotenv = Dotenv.load()
 
-  System.setProperty("SERVER_PORT", dotenv["SERVER_PORT"])
-  System.setProperty("STREAM_KEY", dotenv["STREAM_KEY"])
-  System.setProperty("GROUP_ID", dotenv["GROUP_ID"])
-  System.setProperty("ASSET_SERVICE_URL", dotenv["ASSET_SERVICE_URL"])
-  System.setProperty("REDIS_HOST", dotenv["REDIS_HOST"])
+  val profile = dotenv["SPRING_PROFILES_ACTIVE"] ?: "local"
+  System.setProperty("spring.profiles.active", profile)
+
+  if (profile == "local") {
+    System.setProperty("SERVER_PORT", dotenv["SERVER_PORT"])
+    System.setProperty("STREAM_KEY_FORMAT", dotenv["STREAM_KEY_FORMAT"])
+    System.setProperty("STREAM_KEY_VALIDATE", dotenv["STREAM_KEY_VALIDATE"])
+    System.setProperty("GROUP_ID", dotenv["GROUP_ID"])
+    System.setProperty("ASSET_SERVICE_URL", dotenv["ASSET_SERVICE_URL"])
+    System.setProperty("REDIS_HOST", dotenv["REDIS_HOST"])
+  }
 
   runApplication<Parse>(*args)
 }
