5d8b365dddf50f57010001278bffa6e144bd0860
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-08 12:31:25 -0300

    fix: remove dotEnv

--- src/main/kotlin/com/ingsis/parse/Parse.kt
+++ src/main/kotlin/com/ingsis/parse/Parse.kt
@@ -1,6 +1,5 @@
 package com.ingsis.parse
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
@@ -9,19 +8,5 @@ import org.springframework.boot.runApplication
 open class Parse
 
 fun main(args: Array<String>) {
-  val dotenv = Dotenv.load()
-
-  val profile = dotenv["SPRING_PROFILES_ACTIVE"] ?: "local"
-  System.setProperty("spring.profiles.active", profile)
-
-  if (profile == "local") {
-    System.setProperty("SERVER_PORT", dotenv["SERVER_PORT"])
-    System.setProperty("STREAM_KEY_FORMAT", dotenv["STREAM_KEY_FORMAT"])
-    System.setProperty("STREAM_KEY_VALIDATE", dotenv["STREAM_KEY_VALIDATE"])
-    System.setProperty("GROUP_ID", dotenv["GROUP_ID"])
-    System.setProperty("ASSET_SERVICE_URL", dotenv["ASSET_SERVICE_URL"])
-    System.setProperty("REDIS_HOST", dotenv["REDIS_HOST"])
-  }
-
   runApplication<Parse>(*args)
 }
