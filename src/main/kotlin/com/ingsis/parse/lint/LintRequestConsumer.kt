4fd65c1513d48c8b30c01c9b7d48e8c53e838569
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-14 16:12:27 -0300

    async fixes?

--- src/main/kotlin/com/ingsis/parse/lint/LintRequestConsumer.kt
+++ src/main/kotlin/com/ingsis/parse/lint/LintRequestConsumer.kt
@@ -5,6 +5,7 @@ import com.ingsis.parse.async.JsonUtil
 import com.ingsis.parse.language.PrintScript
 import kotlinx.coroutines.runBlocking
 import org.austral.ingsis.redis.RedisStreamConsumer
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.connection.stream.ObjectRecord
@@ -21,19 +22,30 @@ class LintRequestConsumer @Autowired constructor(
   private val assetService: AssetService
 ) : RedisStreamConsumer<String>(streamRequestKey, groupId, redis) {
 
+  private val logger = LoggerFactory.getLogger(RedisStreamConsumer::class.java)
+
   override fun onMessage(record: ObjectRecord<String, String>) {
-    val formatRequest = JsonUtil.deserializeLintRequest(record.value)
-    val lintingRules = JsonUtil.deserializeLintingRules(assetService.getAssetContent(formatRequest.author, "LintingRules"))
-    val result = PrintScript.lint(formatRequest.snippet, "1.1", lintingRules)
+    try {
+      val formatRequest = JsonUtil.deserializeLintRequest(record.value)
 
-    val response = if (result.isEmpty()) {
-      LintResponse(formatRequest.requestId, "compliant")
-    } else {
-      LintResponse(formatRequest.requestId, "non-compliant")
-    }
+      val assetContent = assetService.getAssetContent(formatRequest.author, "LintingRules")
 
-    runBlocking {
-      linterResponseProducer.publishEvent(JsonUtil.serializeLintResponse(response))
+      val lintingRules = JsonUtil.deserializeLintingRules(assetContent)
+
+      val result = PrintScript.lint(formatRequest.snippet, "1.1", lintingRules)
+      logger.info("Lint result is: {}", result)
+
+      val response = if (result.isEmpty()) {
+        LintResponse(formatRequest.requestId, "compliant")
+      } else {
+        LintResponse(formatRequest.requestId, "non-compliant")
+      }
+
+      runBlocking {
+        linterResponseProducer.publishEvent(JsonUtil.serializeLintResponse(response))
+      }
+    } catch (e: Exception) {
+      logger.error("Error processing message: ${e.message}", e)
     }
   }
 
